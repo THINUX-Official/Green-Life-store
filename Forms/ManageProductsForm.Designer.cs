3194e6d254c993003786c6d7f39c5ac8b80eea95
THINUX-Official <thinuxofficial@gmail.com>
2026-01-18 18:38:27 +0530

    Product update functions created.

--- Forms/ManageProductsForm.Designer.cs
+++ Forms/ManageProductsForm.Designer.cs
@@ -31,6 +31,8 @@
             this.dgvProducts = new System.Windows.Forms.DataGridView();
             this.btnBack = new System.Windows.Forms.Button();
             this.btnAddProduct = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,13 +45,13 @@
             this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvProducts.Location = new System.Drawing.Point(12, 41);
             this.dgvProducts.Name = "dgvProducts";
-            this.dgvProducts.Size = new System.Drawing.Size(776, 368);
+            this.dgvProducts.Size = new System.Drawing.Size(1160, 599);
             this.dgvProducts.TabIndex = 0;
             // 
             // btnBack
             // 
             this.btnBack.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnBack.Location = new System.Drawing.Point(713, 415);
+            this.btnBack.Location = new System.Drawing.Point(1097, 646);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(75, 23);
             this.btnBack.TabIndex = 1;
@@ -59,8 +61,8 @@
             // 
             // btnAddProduct
             // 
-            this.btnAddProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAddProduct.Location = new System.Drawing.Point(698, 12);
+            this.btnAddProduct.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddProduct.Location = new System.Drawing.Point(1082, 12);
             this.btnAddProduct.Name = "btnAddProduct";
             this.btnAddProduct.Size = new System.Drawing.Size(90, 23);
             this.btnAddProduct.TabIndex = 2;
@@ -68,11 +70,35 @@
             this.btnAddProduct.UseVisualStyleBackColor = true;
             this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
             // 
+            // btnDelete
+            // 
+            this.btnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDelete.Location = new System.Drawing.Point(890, 12);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "- Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDeleteProduct_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUpdate.Location = new System.Drawing.Point(986, 12);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(90, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdateProduct_Click);
+            // 
             // ManageProductsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1184, 681);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAddProduct);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.dgvProducts);
@@ -90,5 +116,7 @@
         private System.Windows.Forms.DataGridView dgvProducts;
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
     }
 }
--- Forms/ManageProductsForm.cs
+++ Forms/ManageProductsForm.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using GreenLifeStore.Models;
+using MySql.Data.MySqlClient;
 using System;
 using System.Data;
 using System.Windows.Forms;
@@ -24,7 +25,7 @@ namespace GreenLifeStore.Forms
             {
                 using (MySqlConnection connection = new MySqlConnection(connectionString))
                 {
-                    string query = "select * from products";
+                    string query = "SELECT product_id, product_name, category, price, stock_quantity, supplier, discount FROM products WHERE active_status = 1";
 
                     MySqlCommand cmd = new MySqlCommand(query, connection);
                     MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
@@ -67,6 +68,71 @@ namespace GreenLifeStore.Forms
         {
             AddProductForm addProductForm = new AddProductForm(this);
             addProductForm.Show();
+            this.Hide();
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a product to delete.");
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to remove this product?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning
+            );
+
+            if (result == DialogResult.No) return;
+
+            int productId = Convert.ToInt32(dgvProducts.SelectedRows[0].Cells["product_id"].Value);
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = @"
+                    UPDATE products 
+                    SET active_status = 0, 
+                    modified_date = NOW() 
+                    WHERE product_id = @id";
+
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.Parameters.AddWithValue("@id", productId);
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadProducts();
+            MessageBox.Show("Product removed successfully.");
+        }
+
+        private void btnUpdateProduct_Click(object sender, EventArgs e)
+        {
+            if (dgvProducts.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Please select a product to update.");
+                return;
+            }
+
+            DataGridViewRow row = dgvProducts.SelectedRows[0];
+
+            Product selectedProduct = new Product
+            {
+                Id = Convert.ToInt32(row.Cells["product_id"].Value),
+                Name = row.Cells["product_name"].Value.ToString(),
+                Category = row.Cells["category"].Value.ToString(),
+                Price = Convert.ToDouble(row.Cells["price"].Value),
+                StockQty = Convert.ToInt32(row.Cells["stock_quantity"].Value),
+                Supplier = row.Cells["supplier"].Value.ToString(),
+                Discount = Convert.ToInt32(row.Cells["discount"].Value)
+            };
+
+            UpdateProductForm updateForm = new UpdateProductForm(this, selectedProduct);
+
+            updateForm.Show();
             this.Hide();
         }
     }
